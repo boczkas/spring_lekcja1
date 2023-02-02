5ae2786c68a4af0131d3253981cedd1a96d4eeca
Przemek <przemyslawjakubowski@gmail.com>
2023-02-02 11:53:05 +0100

    Na lekcje Spring 1

--- src/main/java/com/przemek/simple/hello/HelloController.java
+++ src/main/java/com/przemek/simple/hello/HelloController.java
@@ -19,7 +19,6 @@ public class HelloController {
         return "Siema";
     }
 
-//    Zadanie:
 //    Dopisz endpoint, który obsłuży zapytania wysyłane na adres http://localhost:8080/elo
 //    Ma odpowiedzieć tekstem "Elo!"
 //    Sprawdź czy wszystko działa wchodząc na http://localhost:8080/elo
@@ -31,9 +30,24 @@ public class HelloController {
         return "Siema " + imie;
     }
 
-//    Zadanie:
-//    Dopisz endpoint, który obsłyży zapytania wysyłane na adres http://localhost:8080/elo/mordeczka
+//    Dopisz endpoint, który obsłuży zapytania wysyłane na adres http://localhost:8080/elo/mordeczka
 //    Ma odpowiedzieć tekstem "Elo mordeczko Przemek" dla przykładowego zapytania:
 //    http://localhost:8080/elo/Przemek
 
+    @GetMapping("/dodaj/{liczba1}/{liczba2}")
+    public int dodaj(@PathVariable int liczba1, @PathVariable int liczba2) {
+        int suma = liczba1 + liczba2;
+        return suma;
+    }
 }
+
+
+// Zadanie domowe:
+// Napisz endpoint który będzie zwracał słowo wypisane konkretną ilość razy
+// Jego ścieżka: http://localhost:8080/wypisz/slowo/5
+// Wynikiem takiego wywolania powinno być wypisanie na stronie:
+//  slowo
+//  slowo
+//  slowo
+//  slowo
+//  slowo
